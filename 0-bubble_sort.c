83b517bd5c5c2ce9aa80792abb76804003f69e8e
MubarakXBT <kazeemmubarak@gmail.com>
2024-01-01 02:04:14 +0100

    task 0: A c-program to bubble sort a given array

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,16 +1,24 @@
 #include "sort.h"
+/**
+ * bubble_sort - A program to bubble sort an array
+ * @array: the set of unsorted arrays
+ * @size: size of the array
+ *
+ * Return: Void
+*/
 void bubble_sort(int *array, size_t size)
 {
 	size_t temp, i, n = 0;
+
 	while (n < size)
 	{
-		for (i = 0; i < size; i++)
+		for (i = 0; i < size - 1; i++)
 		{
-			if (array[i] > array[i+1])
+			if (array[i] > array[i + 1])
 			{
 				temp = array[i];
-				array[i] = array[i+1];
-				array[i+1] = temp;
+				array[i] = array[i + 1];
+				array[i + 1] = temp;
 				print_array(array, size);
 			}
 		}
